ca99f791cd8362043e89b4700681e478d4c7f26d
lajaodk <153519891+lajaodk@users.noreply.github.com>
2024-10-18 07:36:15 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasília'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua japonesa',
+    'Como se diz obrigado em japonês?',
+    'obrigado em japonês é Arigatô'
+)
+
